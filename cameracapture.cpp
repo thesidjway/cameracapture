752a88aac81331708b6ac5b99a0f5d53ce3c9067
zafrullahsyed <zafrullahmehdi@gmail.com>
2013-10-15 13:16:02 +0200

    capturing a single frame

--- cameracapture.cpp
+++ cameracapture.cpp
@@ -5,17 +5,9 @@ using namespace cv;
 
 int main(int, char**)
 {
-    VideoCapture cap(0); // open the default camera
 
-Mat frame;
-    namedWindow("feed",1);
-//    for(;;)
-{
-    Mat frame;
-    cap >> frame;   // get a new frame from camera
-    //imshow("feed", frame);
-imwrite("output.png", frame);    
-//if(waitKey(1) >= 0) break;
-}
-    return 0;
+CvCapture *capture = cvCaptureFromCAM(CV_CAP_ANY);
+IplImage *frame = cvQueryFrame(capture);
+cvSaveImage("output.png", frame);
+return 0;
 }
